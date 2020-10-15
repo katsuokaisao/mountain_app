d46e582d4339d6eb4ba1fbfc8f363d833efd9079
katsuokaisao <isaoda27@gmail.com>
2020-10-15 13:06:13 +0900

    dropdownの簡略的に実装

--- app/javascript/packs/applicationjs.js
+++ app/javascript/packs/applicationjs.js
@@ -1,3 +1,10 @@
-// document.addEventListener('DOMContentLoaded', function() {
-//   const nodelist = document.querySelectorAll()
-// });
+const dropdownTrigger = document.getElementById('dropdown-trigger');
+const dropdown = document.getElementById('dropdown');
+console.log(dropdown.className);
+dropdownTrigger.addEventListener('click', function() {
+  if(dropdown.classList.contains('is-active') === true ) {
+    dropdown.classList.remove('is-active');
+  }else {
+    dropdown.classList.add('is-active');
+  } 
+});
