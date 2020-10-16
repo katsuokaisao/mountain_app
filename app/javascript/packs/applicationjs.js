e4b8cc16f51b26b0762ddecfb551f36d2bbf6088
katsuokaisao <isaoda27@gmail.com>
2020-10-16 13:43:45 +0900

    cssの整形とログアウト後のaddEventLitenerのエラーの解決

--- app/javascript/packs/applicationjs.js
+++ app/javascript/packs/applicationjs.js
@@ -1,6 +1,5 @@
 const dropdownTrigger = document.getElementById('dropdown-trigger');
 const dropdown = document.getElementById('dropdown');
-console.log(dropdown.className);
 dropdownTrigger.addEventListener('click', function() {
   if(dropdown.classList.contains('is-active') === true ) {
     dropdown.classList.remove('is-active');
