0ebc6eb94ab449fb2ada1321c09f902eb1be2d9c
katsuokaisao <isaoda27@gmail.com>
2020-10-18 13:32:52 +0900

    validationの追加
    validationの追加とエラー分の追加、エラー分の日本語化を行った。

--- app/models/daily.rb
+++ app/models/daily.rb
@@ -1,4 +1,7 @@
 class Daily < ApplicationRecord
   belongs_to :user
   belongs_to :mountain
+  validates :mountain_name, presence: true, length: {maximum: 30}
+  validates :title, presence: true, length: {maximum: 30}
+  validates :comment, presence: true, length: {maximum: 600}
 end
