58c6b0639997fba744c717e2903af93a3ade478f
katsuokaisao <isaoda27@gmail.com>
2020-10-30 11:06:39 +0900

    unicornファイル名の変更
    ECSのタスクの実行でunicornの起動の引数エラーが出ていて引数の内設定ファイルの指定がうまくいっていなさそうなのでファイル名を変更してみる

--- config/unicorn-pro.conf.rb
+++ config/unicorn-pro.conf.rb
@@ -17,7 +17,7 @@ timeout $timeout
 preload_app true
 # before starting processes
 before_fork do |server, worker|
-  defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
+  defined?(ActiveRecord::Base) || ActiveRecord::Base.connection.disconnect!
   old_pid = "#{server.config[:pid]}.oldbin"
   if old_pid != server.pid
     begin
@@ -28,5 +28,5 @@ before_fork do |server, worker|
 end
 # after finishing processes
 after_fork do |server, worker|
-  defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
+  defined?(ActiveRecord::Base) || ActiveRecord::Base.establish_connection
 end
