163440db0f3783179d460b697b48a67bc862ab91
katsuokaisao <isaoda27@gmail.com>
2020-11-01 15:55:29 +0900

    unicornの設定ファイルの変更

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -13,17 +13,17 @@ stdout_path File.expand_path 'log/strdout_path', $app_dir
 timeout $timeout
 preload_app true
 
-before_fork do |server, worker|
-  defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
-  old_pid = "#{server.config[:pid]}.oldbin"
-  if old_pid != server.pid
-    begin
-      sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
-      Process.kill(sig, File.read(old_pid).to_i)
-    rescue Errno::ENOENT, Errno::ESRCH
-    end
-  end
-end
-after_fork do |server, worker|
-  defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
-end
+# before_fork do |server, worker|
+#   defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
+#   old_pid = "#{server.config[:pid]}.oldbin"
+#   if old_pid != server.pid
+#     begin
+#       sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
+#       Process.kill(sig, File.read(old_pid).to_i)
+#     rescue Errno::ENOENT, Errno::ESRCH
+#     end
+#   end
+# end
+# after_fork do |server, worker|
+#   defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
+# end
