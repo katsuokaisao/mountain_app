d68008ed8d0f3740667264d0b333264625ccccf8
katsuokaisao <isaoda27@gmail.com>
2020-11-01 23:14:08 +0900

    unicornの設定ファイルの修正

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -8,8 +8,8 @@ listen $listen
 pid $pid
 worker_processes $worker
 working_directory $app_dir
-stderr_path File.expand_path 'log/strderr_path', $app_dir
-stdout_path File.expand_path 'log/strdout_path', $app_dir
+# stderr_path File.expand_path 'log/strderr_path', $app_dir
+# stdout_path File.expand_path 'log/strdout_path', $app_dir
 # stdout_path $stderr
 # stdout_path $stdout
 # stderr_path File.expand_path('../../log/unicorn_stderr.log', __FILE__)
