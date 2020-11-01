e94ce3c02705ed172ad7ac9cdc8ddc473dec5768
katsuokaisao <isaoda27@gmail.com>
2020-11-01 18:12:23 +0900

    unicornの設定ファイルを直したところ進展した。

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -4,26 +4,35 @@ $app_dir = "/mountain_app"
 $listen  = 3000
 $pid     = File.expand_path 'tmp/pids/unicorn.pid', $app_dir
 
-listen  $listen
+listen $listen
 pid $pid
-worker_processes  $worker
+worker_processes $worker
 working_directory $app_dir
-stderr_path File.expand_path 'log/strderr_path', $app_dir
-stdout_path File.expand_path 'log/strdout_path', $app_dir
+# stderr_path File.expand_path 'log/strderr_path', $app_dir
+# stdout_path File.expand_path 'log/strdout_path', $app_dir
+# stdout_path $stderr
+# stdout_path $stdout
+# stderr_path File.expand_path('../../log/unicorn_stderr.log', __FILE__)
+# stdout_path File.expand_path('../../log/unicorn_stdout.log', __FILE__)
+
 timeout $timeout
+
+# loading booster
 preload_app true
 
-# before_fork do |server, worker|
-#   defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
-#   old_pid = "#{server.config[:pid]}.oldbin"
-#   if old_pid != server.pid
-#     begin
-#       sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
-#       Process.kill(sig, File.read(old_pid).to_i)
-#     rescue Errno::ENOENT, Errno::ESRCH
-#     end
-#   end
-# end
-# after_fork do |server, worker|
-#   defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
-# end
+# before starting processes
+before_fork do |server, worker|
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
+  old_pid = "#{server.config[:pid]}.oldbin"
+  if old_pid != server.pid
+    begin
+      Process.kill "QUIT", File.read(old_pid).to_i
+    rescue Errno::ENOENT, Errno::ESRCH
+    end
+  end
+end
+
+# after finishing processes
+after_fork do |server, worker|
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
+end
