aed3c2ec38cfd4db1250bcd1467f4a044a7de0c5
katsuokaisao <isaoda27@gmail.com>
2020-11-01 11:02:52 +0900

    nginxとunicornの設定を本番環境になるべく近づけて動作することを確認した。

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,6 @@
 Rails.application.configure do
+  config.hosts << "unicorn"
+
   config.web_console.whitelisted_ips = '0.0.0.0/0'
   # Settings specified here will take precedence over those in config/application.rb.
 
