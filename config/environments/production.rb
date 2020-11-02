57a64fdf93d9dba7a4cb6b9ea716ff2515c3edac
katsuokaisao <isaoda27@gmail.com>
2020-11-02 13:31:17 +0900

    本番に近い環境の開発環境でのdocker-compose up -dの成功

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,8 +16,9 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
-
+  # config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.public_file_server.enabled = true
+  
   # アセットパイプラインを有効にするかどうかを指定します。
   config.assets.enabled = true
   # デバッグ用にアセットの連結と圧縮をやめるかどうかを指定します。
