9c8e1b84d1f776514d88a163d038474c2d744126
katsuokaisao <isaoda27@gmail.com>
2020-11-06 15:02:12 +0900

    unicornのファイルの出力の設定

--- EC2/unicorn.rb
+++ EC2/unicorn.rb
@@ -1,13 +1,14 @@
 $worker  = 2
 $timeout = 30 
 $listen  = 3000
+$app_dir = "~/mountain_app"
 $pid     = File.expand_path 'tmp/pids/unicorn.pid', $app_dir
 listen $listen
 pid $pid
 worker_processes $worker
 working_directory $app_dir
-stderr_path File.expand_path('../../tmp/log/stderr.log', __FILE__)
-stdout_path File.expand_path('../../tmp/log/stdout.log', __FILE__)
+stderr_path File.expand_path('../../tmp/log/stderr.log', $app_dir)
+stdout_path File.expand_path('../../tmp/log/stdout.log', $app_dir)
 timeout $timeout
 preload_app true
 
