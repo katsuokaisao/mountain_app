752fd3667a352fe2bf689be51e85c14b0e2f15ab
katsuokaisao <isaoda27@gmail.com>
2020-11-13 15:23:40 +0900

    テストデータの修正

--- spec/factories/dailies.rb
+++ spec/factories/dailies.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :daily do
-    mountain_name { "MyString" }
-    title { "MyString" }
-    comment { "MyText" }
-    user { nil }
-    mountain { nil }
+    mountain_name { "富士山" }
+    title { "絶景富士山に行ってみた" }
+    comment { "道のりがとても険しくて大変だと思ってたけど、道が整備されていてとても楽に登ることができた。朝日はとても綺麗でこれがご来光って言うものかと感嘆した。皆さんにも富士山は是非お勧めしたいです。" }
+    association :user
+    association :mountain
   end
 end
