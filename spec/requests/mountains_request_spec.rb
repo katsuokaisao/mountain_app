345a702d846919269224645e44d420f0b55672e4
katsuokaisao <isaoda27@gmail.com>
2020-11-14 18:20:28 +0900

    request specのエラーの解決

--- spec/requests/mountains_request_spec.rb
+++ spec/requests/mountains_request_spec.rb
@@ -4,14 +4,14 @@ RSpec.describe "Mountains", type: :request do
 
   describe "GET /index" do
     it "returns http success" do
-      get "/mountains/index"
+      get mountains_path
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "GET /show" do
     it "returns http success" do
-      get "/mountains/show"
+      get mountain_path(50)
       expect(response).to have_http_status(:success)
     end
   end
