1b4841cc78683a4adfbf3e04abb0be69ba2b8d2f
katsuokaisao <isaoda27@gmail.com>
2020-11-16 09:19:04 +0900

    自分自身をフォローできないようにした

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -3,7 +3,7 @@ class RelationshipsController < ApplicationController
   def create
     @user = current_user
     @other_user = User.find_by(id: params[:followed_id])
-    current_user.follow(@other_user)
+    current_user.follow(@other_user) unless @user == @other_user
     respond_to do |format|
       format.html {  redirect_back fallback_location: following_user_path(@user.id) }
       format.js
@@ -13,7 +13,7 @@ class RelationshipsController < ApplicationController
   def destroy
     @user = User.find_by(params[:id])
     @other_user = User.find_by(id: params[:followed_id])
-    @user.unfollow(@other_user)
+    @user.unfollow(@other_user) unless @user == @other_user
     respond_to do |format|
       format.html { redirect_back fallback_location: following_user_path(@user.id) }
       format.js
