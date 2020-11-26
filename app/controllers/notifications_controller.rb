3a7d5a0551b4ea7df93306f41380a8b3945697e9
katsuokaisao <isaoda27@gmail.com>
2020-11-26 20:23:57 +0900

    通知が表示されないバグの修正

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -2,7 +2,7 @@ class NotificationsController < ApplicationController
   before_action :authenticate_user!
   
   def index 
-    @notifications = current_user.passive_notifications.eager_load(:visitor, :visited, :daily).where(visitor_id: current_user.id).page(params[:page]).per(8)
+    @notifications = current_user.passive_notifications.eager_load(:visited, :daily).preload(visitor: :profile).where(visited_id: current_user.id).page(params[:page]).per(8)
     @notifications.where(checked: false).each do |notification|
       notification.update(checked: true)
     end
