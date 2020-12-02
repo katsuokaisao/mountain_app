6c652042a2574594edddf8eeed90cb4e6c47d724
katsuokaisao <isaoda27@gmail.com>
2020-12-02 18:04:31 +0900

    チャットルームとメッセージ機能の大枠の完成
    room controller と message controllerの作成と簡単なビューの作成、stylesheetの準備、モデルの修正

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,18 @@
 class MessagesController < ApplicationController
   def create
+    if Entry.where(user_id: current_user.id, room_id: params[:message][:room_id]).present?
+      @message = current_user.messages.create(message_params)
+      redirect_to room_path @message.room_id
+    else
+      flash[:alert] = "メッセージの送信に失敗しました！"
+    end
   end
 
   def destroy
   end
+
+  private 
+    def message_params 
+      params.require(:message).permit(:text, :room_id)
+    end
 end
