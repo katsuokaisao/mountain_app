db3e7c9c3ac0b3f7dafb885ab585205abfd74b90
katsuokaisao <isaoda27@gmail.com>
2020-12-03 14:08:26 +0900

    メッセージの作成と削除のajax対応

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,17 +1,27 @@
 class MessagesController < ApplicationController
+  # protect_from_forgery :except => [:destroy]
   def create
-    if Entry.where(user_id: current_user.id, room_id: params[:message][:room_id]).present?
-      @message = current_user.messages.create(message_params)
-      redirect_to room_path @message.room_id
+    entry = Entry.where(user_id: current_user.id, room_id: params[:message][:room_id])
+    if entry.present?
+      current_user.messages.create(message_params)
+      @messages = entry.first.room.messages
     else
       flash[:alert] = "メッセージの送信に失敗しました！"
+    end
+    respond_to do |format|
+      format.html { redirect_back fallback_location: root_path }
+      format.js
     end
   end
 
   def destroy
     message = Message.find(params[:id])
     message.destroy if message.present?
-    redirect_back(fallback_location: room_path(message.room.id))
+    @messages = message.room.messages
+    respond_to do |format|
+      format.html {     redirect_back(fallback_location: room_path(message.room.id)) }
+      format.js
+    end
   end
 
   private 
